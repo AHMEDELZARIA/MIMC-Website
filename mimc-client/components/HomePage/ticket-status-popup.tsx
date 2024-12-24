4737f6ebd7948dad9a4730f9fb2e67aa4d5ebccb
Ahmed Elzaria <aelzaria@gmail.com>
2024-12-24 12:40:26 -0500

    added ticket purchase confirmation pop-ups

---
+++ mimc-client/components/HomePage/ticket-status-popup.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const TicketStatusPopup: React.FC = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const success = searchParams.get("success");
+    const canceled = searchParams.get("canceled");
+
+    if (success === "true") {
+      toast.success(
+        "JazakAllahu khairan for purchasing your ticket(s)! We look forward to hosting you! Please check your email for your receipt in a few minutes.",
+        {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+      });
+    } else if (canceled === "true") {
+      toast.error(
+        "Your ticket purchase did not go through and was canceled. Please try again. If issue persists please contact us via email.",
+        {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+      });
+    }
+
+    // Clear query parameters
+    if (success || canceled) {
+      router.replace("/", { shallow: true });
+    }
+  }, [searchParams, router]);
+
+  return <ToastContainer />;
+};
+
+export default TicketStatusPopup;
