108581f635aab2f6601f7113f3beba6ea5571f8c
Ahmed Raja <ahmediqbalraja@gmail.com>
2025-01-12 16:30:37 -0500

    update activities for release

--- mimc-client/components/ProgramPage/activities.tsx
+++ mimc-client/components/ProgramPage/activities.tsx
@@ -7,59 +7,98 @@ import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline"; // Import ad
 
 import { cinzel } from "@/config/fonts";
 import { title } from "process";
+import { link } from "fs";
 
 export default function Activities() {
   // Activities Data
   const activities = [
-    // {
-    //   title: "QIYAM",
-    //   category: "ACCOMMODATIONS",
-    //   imageUrl: "/imgs/com/c33.jpg",
-    //   date: "Feb 15, 2025",
-    //   time: "11:00 AM - 4:30 PM",
-    //   requiresSignUp: true,
-    // },
-    // {
-    //   title: "SHARK TANK",
-    //   category: "CASE COMPETITION",
-    //   imageUrl: "/imgs/act/act22.jpg",
-    //   date: "Feb XX, 2024",
-    //   time: "2:00 PM - 5:00 PM",
-    //   requiresSignUp: true,
-    // },
+    {
+      title: "QIYAM NIGHT",
+      category: "ACCOMMODATIONS",
+      imageUrl: "/imgs/com/c33.jpg",
+      date: "Sat Feb 14, 2025",
+      time: "9:00 PM - 8:00 AM",
+      requiresSignUp: true,
+      link: "https://docs.google.com/forms/d/e/1FAIpQLScSY9QW0pA2dmAtre7kUIHpryEL04PQp3IOeJHS4EkI-Btuqw/viewform?usp=sharing"
+    },
+    {
+      title: "SHARK TANK",
+      category: "COMPETITION & WORKSHOP",
+      imageUrl: "/imgs/act/act22.jpg",
+      date: "Sun Feb 16, 2025",
+      time: "12:00 PM - 3:30 PM",
+      requiresSignUp: true,
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSfMpGn5-Yb5NsT6PRsvEenSqIfqD5cqyXCLIgiqv2m4Q9GQ-Q/viewform"
+    },
     {
       title: "QIRAA'ATUL",
       category: "QUR'AN COMPETITION",
       imageUrl: "/imgs/act/act19.jpg",
-      date: "Feb 15, 2025",
-      time: "12:00 PM - 4:30 PM",
+      date: "Sat Feb 15, 2025",
+      time: "12:00 PM - 4:00 PM",
       requiresSignUp: true,
       link: "https://docs.google.com/forms/d/e/1FAIpQLScBDBGLH67Mqlzqg35h8xEQOaf0LabSYNzFqMMgr5j0ckbTeA/viewform?usp=dialog",
     },
-    // {
-    //   title: "STAINED",
-    //   category: "GLASS MAKING",
-    //   imageUrl: "/imgs/act/stained-glass.jpg",
-    //   date: "Dec 13, 2024",
-    //   time: "7:00 PM - 9:00 PM",
-    //   requiresSignUp: true,
-    // },
-    // {
-    //   title: "ESCAPE ROOM",
-    //   category: "TEST YOUR WITS",
-    //   imageUrl: "/imgs/act/escape-room.png",
-    //   date: "Dec 14, 2024",
-    //   time: "4:00 PM - 5:30 PM",
-    //   requiresSignUp: true,
-    // },
-    // {
-    //   title: "CALLIGRAPHY",
-    //   category: "MASTER THE ART OF WRITING",
-    //   imageUrl: "/imgs/act/act4.jpg",
-    //   date: "Dec 15, 2024",
-    //   time: "11:00 AM - 1:00 PM",
-    //   requiresSignUp: false,
-    // },
+    {
+      title: "FAUX STAINED",
+      category: "GLASS MAKING",
+      imageUrl: "/imgs/act/stained-glass.jpg",
+      date: "Sat Feb 15, 2025",
+      time: "2:00 PM - 4:30 PM",
+      requiresSignUp: true,
+      link: "https://docs.google.com/forms/d/1AOjYALVt3Gx007jWrKriwCtTQ7qAc9XEoxW07IF-2Pc/prefill",
+    },
+    {
+      title: "ESCAPE ROOM",
+      category: "TEST YOUR WITS",
+      imageUrl: "/imgs/act/escape-room.png",
+      date: "Sun Feb 16, 2025",
+      time: "1:30 PM - 6:15 PM",
+      requiresSignUp: true,
+      link: "https://docs.google.com/forms/d/1hMh2SH41CUmwhs55DuzYG91BF3pvaEguYEXg-rVlczQ/prefill",
+    },
+    {
+      title: "RAMADAN",
+      category: "WORKSHOP",
+      imageUrl: "/imgs/act/ramadan.png",
+      date: "Sun Feb 16, 2025",
+      time: "5:15 PM - 6:15 PM",
+      requiresSignUp: false,
+    },
+    {
+      title: "INTER-MSA",
+      category: "EXEC FORUM",
+      imageUrl: "/imgs/act/inter-msa.jpg",
+      date: "Sat Feb 15, 2025",
+      time: "12:00 PM - 1:00 PM",
+      requiresSignUp: false,
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSdh537_or0Xho97_PAvDwDf6NAz-kAOzKysrRzYmvTJ8UmhWg/viewform"
+    },
+    {
+      title: "NETWORKING PROGRAM",
+      category: "CAMPUS TO CAREER",
+      imageUrl: "/imgs/act/networking.jpg",
+      date: "Sun Feb 16, 2025",
+      time: "11:00 AM - 1:00 PM",
+      requiresSignUp: false,
+    },
+    {
+      title: "AL FURQAAN",
+      category: "ISLAMIC TRIVIA",
+      imageUrl: "/imgs/act/trivia.jpg",
+      date: "Sun Feb 16, 2025",
+      time: "5:15 PM - 6:15 PM",
+      requiresSignUp: false,
+    },
+    {
+      title: "SURAH AL FATIHA",
+      category: "BOOK DISCUSSION",
+      imageUrl: "/imgs/act/book.jpg",
+      date: "Sat Feb 15, 2025",
+      time: "5:00 PM - 6:00 PM",
+      requiresSignUp: true,
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSdBs3ZkYtD9LcUW7zDKevJAjR_aR2BKBQboi_5_etBAMGfdUA/viewform?usp=sharing",
+    },
   ];
 
   const containerVariants = {
@@ -102,25 +141,13 @@ export default function Activities() {
           <span className="absolute left-1/2 bottom-0 h-[3px] w-0 bg-[#F0FFC9] transition-all duration-500 ease-in-out group-hover:w-full group-hover:left-0" />
         </span>
       </h1>
-      {/* <p className="text-sm md:text-base font-medium text-[#F0FFC9] opacity-80 mb-4 text-justify px-4 md:px-8">
+      <p className="text-sm md:text-base font-medium text-[#F0FFC9] opacity-80 mb-4 text-justify px-4 md:px-8">
         Explore a variety of exciting activities designed to inspire, engage,
         and connect you with fellow attendees. From creative workshops to
-        thrilling challenges, there's something for everyone to enjoy. Be sure
-        to{" "}
+        thrilling challenges, there's something for everyone to enjoy. Some activities, indicated by the icons below, require sign up since space is limited. {" "}
         <strong className="underline text-white">
-          click on the activities below to sign up
-        </strong>{" "}
-        and secure your spot.{" "}
-        <strong className="underline text-white">
-          Some activities require signing up, indicated by the icon below, and
-          you must sign up after purchasing your ticket to guarantee your place,
-          as spaces are limited!
+          Click an activity to secure your spot!
         </strong>
-      </p> */}
-      <p className="text-sm md:text-base font-medium text-[#F0FFC9] opacity-80 mb-4 text-center px-4 md:px-8">
-        Aside from the Qiraa’atul Qur’an Comeptition, all other activity
-        sign-ups will be released early January! Please keep checking our social
-        media and your email for updates.{" "}
       </p>
 
       {/* Legend */}
@@ -137,8 +164,8 @@ export default function Activities() {
 
       {/* Activities Grid */}
       <motion.div
-        // className="grid lg:grid-cols-2 xl:grid-cols-3 gap-y-12 py-3 px-4"
-        className="grid gap-y-12 py-3 px-4" // temp while we only have QQC up to keep it centered.
+        className="grid lg:grid-cols-2 xl:grid-cols-3 gap-y-12 py-3 px-4"
+        // className="grid gap-y-12 py-3 px-4" // temp while we only have QQC up to keep it centered.
         style={{
           backgroundImage: `
             linear-gradient(to bottom, #570326 10%,rgba(87, 3, 38, 0.85) 30%, rgba(87, 3, 38, 0.85) 90%, #570326 100%), 
@@ -153,7 +180,7 @@ export default function Activities() {
         {activities.map((activity, index) => (
           <a
             key={index}
-            className="relative cursor-pointer w-80 h-80 mx-auto rounded-full overflow-hidden border-5 border-[#A9DA88] flex flex-col items-center justify-self transition-all duration-400 hover:scale-105 hover:border-[#A9DA88] hover:shadow-2xl hover:shadow-[#A9DA88] active:scale-95 group"
+            className="relative cursor-pointer w-80 h-80 mx-auto rounded-full overflow-hidden border-5 border-[#A9DA88] flex flex-col items-center justify-self transition-all duration-400 hover:border-[#A9DA88] hover:shadow-2xl hover:shadow-[#A9DA88] active:scale-95 group"
             href={activity.link} // Link to the activity page
             rel="noopener noreferrer" // Security best practice
             target="_blank" // Open in a new tab
@@ -171,7 +198,7 @@ export default function Activities() {
             {/* Overlay Text */}
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4">
               {/* Activity Name */}
-              <h2 className="text-3xl font-bold">{activity.title}</h2>
+              <h2 className="text-2xl font-bold">{activity.title}</h2>
               <h3 className="text-md font-bold bg-[#A9DA88] text-black px-3 py-0.5 rounded-xl mt-2">
                 {activity.category}
               </h3>
